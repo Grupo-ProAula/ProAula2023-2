1f0c5d1f4b65bbe9034865950e593e61b937cf95
Gerson Muñoz <gersonalonsomp44@gmail.com>
2023-11-11 13:25:59 -0500

    Validaciones Generales y cambios en la vista

--- src/main/java/co/edu/unicolombo/ProyectoDeAula20232/Dao/ICrudAsistencias.java
+++ src/main/java/co/edu/unicolombo/ProyectoDeAula20232/Dao/ICrudAsistencias.java
@@ -2,6 +2,7 @@
 package co.edu.unicolombo.ProyectoDeAula20232.Dao;
 
 import co.edu.unicolombo.ProyectoDeAula20232.Models.Asistencias;
+import java.sql.Date;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -18,4 +19,7 @@ public interface ICrudAsistencias extends JpaRepository<Asistencias, Integer>{
     
     @Query("SELECT a FROM Asistencias a WHERE a.estado = 'Activo' AND a.participacion.actividadProgramada.idActividadProgramada = ?1")
     public List<Asistencias> listarAsistenciasActividad(int idActividad);
+    
+    @Query("SELECT a FROM Asistencias a WHERE a.estado = 'Activo' AND a.participacion.actividadProgramada.idActividadProgramada = ?1 AND a.participacion.estudiante.idUsuario = ?2 AND DATE(a.fechaAsistencia) = ?3")
+    public Asistencias verificarAsistencia(int idActividad, int idEstudiante, Date fecha);
 }
